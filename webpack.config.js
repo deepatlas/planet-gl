6642257dfb92539e641128e9781326dd2bc30f74
Lars Haferkamp <lars.haferkamp@comsysto.com>
2020-07-15 20:31:25 +0200

    Move entry point and static content to assets folder for Github pages compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,9 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'assets/javascripts')
   },
   devServer: {
-    contentBase: './dist',
+    contentBase: './assets',
   },
 };
