1572b57b621e9299bf85cf1373b677ea5ad55b05
Lars Haferkamp <lars.haferkamp@comsysto.com>
2020-07-15 21:42:35 +0200

    Small fix in sphere method call

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ document.body.appendChild( renderer.domElement );
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 camera.position.z = 5;
 
-var sphere = createSphere(renderer)
+var sphere = createSphere()
 
 const scene = new THREE.Scene();
 scene.add( sphere );
