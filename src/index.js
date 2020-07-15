2630576a63ff798fd1ed8eb6cab87384025e2f29
Lars Haferkamp <lars.haferkamp@comsysto.com>
2020-07-16 01:08:18 +0200

    Test change and deployment

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-camera.position.z = 5;
+camera.position.z = 6;
 
 var sphere = createSphere()
 
